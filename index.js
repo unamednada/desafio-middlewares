0d1e7e5e14ae22cd511f2812778f9c1f0c068d41
unamednada <dias.gustavo.dias@gmail.com>
2022-01-20 16:55:36 -0300

    Create hellomiddleware

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pingMiddleware = require('./middlewares/ping');
+const helloMiddleware = require('./middlewares/hello');
 
 const app = express();
 const PORT = 3000;
 
-app.use('/ping', pingMiddleware);
+app.use(bodyParser.json());
+
+app.get('/ping', pingMiddleware);
+app.post('/hello', helloMiddleware);
 
 app.listen(PORT, () => { console.log(`App listening on port ${PORT}`)});
