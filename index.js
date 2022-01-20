f21c50fa3e16e32715c5d5a06cd72b93307916c2
unamednada <dias.gustavo.dias@gmail.com>
2022-01-20 17:40:07 -0300

    Finish challenge 06 create simpsons

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const helloMiddleware = require('./middlewares/hello');
 const greetingMiddleware = require('./middlewares/greeting');
 const getSimpsonsMiddleware = require('./middlewares/getSimpsons');
 const getSimpsonByIdMiddleware = require('./middlewares/getSimpsonById');
+const createSimpsonMiddleware = require('./middlewares/createSimpson');
 
 const app = express();
 const PORT = 3000;
@@ -17,5 +18,7 @@ app.post('/greeting', greetingMiddleware);
 
 app.get('/simpson/:id', getSimpsonByIdMiddleware);
 app.get('/simpsons', getSimpsonsMiddleware);
+
+app.post('/simpsons', createSimpsonMiddleware);
 
 app.listen(PORT, () => { console.log(`App listening on port ${PORT}`)});
