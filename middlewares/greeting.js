bb7d5d31c76b6bdfa13c6697c1b31b2764091ffb
unamednada <dias.gustavo.dias@gmail.com>
2022-01-20 17:03:40 -0300

    Create greeting middleware.

---
+++ middlewares/greeting.js
@@ -0,0 +1,9 @@
+const greetingMiddleware = (req, res) => {
+  const { name, age } = req.body;
+  if (age < 18) {
+    return res.status(401).json({ message: 'Unauthorized'});
+  };
+  return res.status(200).json({ message: `Hello, ${name}!` });
+};
+
+module.exports = greetingMiddleware;
